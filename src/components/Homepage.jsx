a083a449c9a3d7d0b3528963dd175afa754798e5
Adelsocial-Vishal <vishalsharma.adelsocial@gmail.com>
2024-07-24 17:55:19 +0530

    button fixed

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -78,11 +78,6 @@ const Homepage = () => {
     <Wrapper>
       {/* <Wrapper> */}
       <Spline scene="https://prod.spline.design/YNlxHU81ZyprXqwS/scene.splinecode" />
-      <Social>
-        <div />
-        {/* <img src={IconTwitter} alt="Twitter" />
-          <img src={IconYouTube} alt="YouTube" /> */}
-      </Social>
       <Content>
         <Box
           sx={{
@@ -171,13 +166,13 @@ const Homepage = () => {
               variant="outlined"
               sx={{
                 float: { lg: "left" },
-                color: "white",
+                color: "black",
                 "&:hover": {
                   background: "white",
                   color: "black",
                 },
                 p: "15px 30px",
-                border: "1px solid white",
+                border: "1px solid black",
                 borderRadius: "100px",
               }}
             >
@@ -762,45 +757,9 @@ const Content = styled.div`
   @media (max-width: 1024px) {
     gap: 40px;
   }
-
-  h1 {
-    font-weight: bold;
-    font-family: "Spline Sans Mono", monospace;
-    font-size: 70px;
-    margin: 0;
-    max-width: 500px;
-    pointer-events: auto;
-    text-shadow: 0 10px 30px rgba(0, 0, 0, 0.5);
-
-    @media (max-width: 1024px) {
-      font-size: 60px;
-      max-width: 400px;
-    }
-    @media (max-width: 800px) {
-      font-size: 40px;
-      max-width: 300px;
-    }
-    @media (max-width: 600px) {
-      padding-top: 250px;
-    }
-  }
-
-  p {
-    font-weight: normal;
-    line-height: 150%;
-    max-width: 380px;
-    pointer-events: auto;
-    text-shadow: 0 10px 30px rgba(0, 0, 0, 0.5);
-  }
-
   button {
-    background: rgba(0, 0, 0, 0.2);
-    border: 0px;
     font-size: 16px;
     padding: 12px 30px;
-    border-radius: 14px;
-    color: white;
-    border: 1px solid rgba(255, 255, 255, 0.1);
     max-width: 280px;
     backdrop-filter: blur(20px);
     font-weight: 600;
@@ -808,12 +767,10 @@ const Content = styled.div`
     transition: 1s;
     cursor: pointer;
     pointer-events: auto;
-
     display: flex;
     gap: 12px;
     justify-content: center;
     align-items: center;
-
     :hover {
       border: 1px solid rgba(255, 255, 255, 0.8);
       transform: translateY(-3px);
@@ -828,80 +785,6 @@ const Content = styled.div`
     @media (max-width: 1024px) {
       margin: 0 30px;
     }
-  }
-`;
-
-const Menu = styled.ul`
-  display: flex;
-  gap: 30px;
-  align-items: center;
-  margin: 0 30px 0 100px;
-  padding: 0;
-  pointer-events: auto;
-
-  @media (max-width: 1024px) {
-    margin: 0 30px;
-  }
-
-  li {
-    list-style: none;
-    margin: 0;
-
-    a {
-      text-decoration: none;
-      color: white;
-      padding: 8px 20px;
-      border-radius: 14px;
-      border: 1px solid rgba(255, 255, 255, 0);
-      transition: 1s;
-
-      :hover {
-        border: 1px solid rgba(255, 255, 255, 0.2);
-      }
-    }
-  }
-
-  button {
-    margin: 0;
-    width: auto;
-    background: rgba(31, 66, 250, 0.2);
-    border: 1px solid rgba(255, 255, 255, 0.4);
-  }
-
-  @media (max-width: 800px) {
-    justify-content: space-between;
-    li:nth-child(2),
-    li:nth-child(3),
-    li:nth-child(4),
-    li:nth-child(5) {
-      display: none;
-    }
-  }
-`;
-
-const Social = styled.div`
-  position: absolute;
-  top: 150px;
-  left: 30px;
-  display: flex;
-  flex-direction: column;
-  gap: 30px;
-  align-items: center;
-
-  @media (max-width: 1024px) {
-    display: none;
-  }
-
-  div {
-    width: 1px;
-    height: 500px;
-    background: linear-gradient(
-      180deg,
-      #08b6f9 0%,
-      #6c56ef 33.57%,
-      #1306dd 65.86%,
-      #aa0eb2 100%
-    );
   }
 `;
 
